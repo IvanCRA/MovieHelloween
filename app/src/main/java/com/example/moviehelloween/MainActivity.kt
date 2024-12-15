dac5fdf7a18a31fdc9f594dd1875f45bff6401f7
IvanCRA <101933328+IvanCRA@users.noreply.github.com>
2024-12-16 00:09:50 +0300

    add Night Theme and fix toolbar

--- app/src/main/java/com/example/moviehelloween/MainActivity.kt
+++ app/src/main/java/com/example/moviehelloween/MainActivity.kt
@@ -19,5 +19,35 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNav.setupWithNavController(navController)
+
+        /*binding.bottomNav.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.homeFragment -> {
+                    binding.toolbar.title = item.title
+                    true
+                }
+                R.id.favouritesFragment -> {
+                    binding.toolbar.title = item.title
+                    true
+                }
+                R.id.searchFragment -> {
+                    binding.toolbar.title = item.title
+                    true
+                }
+                R.id.profileFragment -> {
+                    binding.toolbar.title = item.title
+                    true
+                }
+                else -> false
+            }
+        }*/
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> binding.toolbar.title = getString(R.string.popular_title)
+                R.id.favouritesFragment -> binding.toolbar.title = getString(R.string.favourites_icon)
+                R.id.searchFragment -> binding.toolbar.title = getString(R.string.search_icon)
+                R.id.profileFragment -> binding.toolbar.title = getString(R.string.profile_icon)
+            }
+        }
     }
 }
